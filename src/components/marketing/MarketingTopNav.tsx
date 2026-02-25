e2c596191ea477d51c48e5a0a4438ba1f1b822ef
Leonardo Barbosa <webnardoo@gmail.com>
2026-02-25 19:49:55 -0300

    fix(ui): route app topnav wrapper to new marketing/topnav module

--- src/components/marketing/MarketingTopNav.tsx
+++ src/components/marketing/MarketingTopNav.tsx
@@ -1,9 +1,21 @@
+// src/components/marketing/MarketingTopNav.tsx
 "use client";
 
-import MarketingTopNavInner, { type Props } from "./topnav/MarketingTopNav";
+import React from "react";
 
-export type { Props };
+// ✅ Fonte única do TopNav: módulo novo (mobile-first + notificações)
+import MarketingTopNavNew, { type Props as MarketingTopNavProps } from "./topnav/MarketingTopNav";
 
+export type Props = MarketingTopNavProps;
+
+/**
+ * Wrapper compatível:
+ * Mantém o import existente no app:
+ *   import MarketingTopNav from "@/components/marketing/MarketingTopNav";
+ *
+ * Mas renderiza o TopNav novo (src/components/marketing/topnav/*),
+ * garantindo que o CSS do módulo novo seja carregado.
+ */
 export default function MarketingTopNav(props: Props) {
-  return <MarketingTopNavInner {...props} />;
+  return <MarketingTopNavNew {...props} />;
 }
