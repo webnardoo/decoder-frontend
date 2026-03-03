b7d5fd5923e61cadc967d4e6ee0e579354928be2
Leonardo Barbosa <webnardoo@gmail.com>
2026-03-03 19:15:07 -0300

    fix: allow youtube iframe permissions

---
+++ src/app/app/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'picture-in-picture=*, fullscreen=*, autoplay=*, encrypted-media=*',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
